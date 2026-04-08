5628b0faf87c098b83d1022807913e631866230f
Luis Reyes <alfredluis177@gmail.com>
2026-04-07 22:01:35 -0600

    Instalación de tailwindcss + componente navbar + paginas vacias de libros y formulario creados + ruta app.blade.php en layouts definido como la raiz principal

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,9 @@
+<header class="bg-white shadow">
+    <div class="max-w-7xl flex justify-between items-center mx-auto py-5">
+        <h1 class="font-medium text-xl">Examen Parcial II <i class="bi bi-amd"></i></h1>
+        <nav class="space-x-4 font-medium">
+            <a href="#" class="hover:text-sky-800 transition">Libros</a>
+            <a href="#" class="hover:text-sky-800 transition">Crear</a>
+        </nav>
+    </div>
+</header>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PrimerCrud</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    @vite('resources/css/app.css')
+</head>
+<body class="bg-gray-100 min-h-screen">
+    <x-navbar />
+    @yield('content')
+</body>
+</html>
