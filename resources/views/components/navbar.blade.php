fbabf815125c742dbd2be07041ed429d21096189
Luis Reyes <alfredluis177@gmail.com>
2026-04-07 22:21:02 -0600

    Conexión con base de datos + correción de diseño en navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,5 @@
 <header class="bg-white shadow">
-    <div class="max-w-7xl flex justify-between items-center mx-auto py-5">
+    <div class="max-w-7xl flex justify-between items-center mx-auto py-4">
         <h1 class="font-medium text-xl">Examen Parcial II <i class="bi bi-amd"></i></h1>
         <nav class="space-x-4 font-medium">
             <a href="#" class="hover:text-sky-800 transition">Libros</a>
