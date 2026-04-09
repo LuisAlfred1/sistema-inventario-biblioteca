d46c12b7dccb3195b13953d37ea863613f262b48
Luis Reyes <alfredluis177@gmail.com>
2026-04-08 18:59:05 -0600

    Implementación de métodos en BookController para listar, crear y almacenar libros, incluyendo validaciones y manejo de autores.

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Autor;
+use App\Models\Libro;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
@@ -9,13 +11,33 @@ class BookController extends Controller
     //
     public function index()
     {
-        //Aqui esta retornando la vista de los libros, pero no se esta pasando ningun dato, por lo que no se mostrara nada en la vista
-        return view('pages.books.index');
+        $libros = Libro::with('autor')->get();
+        return view('pages.books.index', compact('libros'));
     }
 
     public function create()
     {
-        //Aqui esta retornando la vista para crear un nuevo libro
-        return view('pages.books.create');
+        //Aqui se esta obteniendo la lista de autores para mostrarla en el formulario de creacion de libros. Se ordenan por nombre para que sea mas facil encontrar el autor deseado.
+        $autores = Autor::orderBy('nombre')->get();
+        //Si el autor esta vacio, se redirige a la pagina de libros con un mensaje de error, indicando que no hay autores disponibles y que se debe crear un autor antes de crear un libro
+        if ($autores->isEmpty()) {
+            return redirect()->route('books.index')->with('error', 'No hay autores disponibles. Por favor, crea un autor antes de crear un libro.');
+        }
+        return view('pages.books.create', compact('autores'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'titulo'           => 'required|string|max:255',
+            //.date('Y') se utiliza para obtener el año actual, y se establece como el valor máximo permitido para el campo 'anio_publicacion', lo que significa que el usuario no podrá ingresar un año de publicación que sea mayor al año actual.
+            'anio_publicacion' => 'nullable|integer|min:1000|max:' . date('Y'),
+            'stock'            => 'required|integer|min:0',
+            'autor_id'         => 'required|exists:autors,id',
+        ]);
+
+        Libro::create($request->only(['titulo', 'anio_publicacion', 'stock', 'autor_id']));
+
+        return redirect()->route('books.index')->with('success', 'Libro creado exitosamente.');
     }
 }
